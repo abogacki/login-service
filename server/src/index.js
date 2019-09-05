65f8f59c6c9d6b8c9501dcb3e09bb40fb7bbe57c
abogacki <andrzej.bogacki@intive.com>
2019-09-05 08:12:48 +0200

    Moves server to separate folder, creates dockerfile for backend

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+const Koa = require('koa');
+const app = new Koa();
+const bodyParser = require('koa-bodyparser');
+const session = require('koa-session');
+const passport = require('koa-passport');
+
+const SECRET_KEY = 'my-uber-secret-key';
+
+app.keys = [SECRET_KEY];
+app.use(session(app));
+app.use(bodyParser());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => console.log(`Server started at ${PORT}`));
+
+module.exports = server;
