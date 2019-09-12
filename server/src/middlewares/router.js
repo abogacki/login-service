1d00d5a72e1c34069e5646a42b98394ec6bcd0b8
abogacki <andrzej.bogacki@intive.com>
2019-09-12 12:29:17 +0200

    Makes signin ang change souter graphql endpoint to allow only authenticated users

--- server/src/middlewares/router.js
+++ server/src/middlewares/router.js
@@ -3,10 +3,21 @@ const router = new Router();
 const jwt = require('./jwt');
 const routes = require('../routes');
 const graphql = require('./koa-graphql');
+const passport = require('koa-passport');
 
 router.get('/', routes.index);
 
-router.all('/graphql', jwt, graphql);
+router.all(
+  '/graphql',
+  async (ctx, next) => {
+    if (ctx.isAuthenticated()) {
+      next();
+    } else {
+      await ctx.render('error', { message: 'Untauthorized' });
+    }
+  },
+  graphql
+);
 
 router.post('/signup', routes.signUpPost);
 router.get('/signup', routes.signUpGet);
